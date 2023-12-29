ec63226a94aa8defdd585b60e372679832ae2c7d
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-12-29 06:17:27 -0800

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,15 @@ A script that starts flask web app
 
 """
 from flask import Flask
+
 app= Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
+
 def hello():
-    """Starts basic Flash web application"""
+    """Starts basic Flask web application"""
     return 'Hello HBNB!'
+
 if __name__ == '__main__':
+    """This runs if not imported"""
     app.run(host='0.0.0.0', port=5000)
